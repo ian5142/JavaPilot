2aba6ef7227463b210e07f6bbab768ef3e537f98
ian5142 <ian@birdisland.net>
2022-02-18 15:41:19 -0400

    Creates an RS232Control object calls it's testRead method to return a character array, converts it to a string.

--- src/javapilot/NMEA0183_IO.java
+++ src/javapilot/NMEA0183_IO.java
@@ -9,5 +9,22 @@ package javapilot;
  * @author Ian
  */
 public class NMEA0183_IO {
+    RS232Control controller;
+    double currentHDG;
     
+    public NMEA0183_IO () {
+        controller = new RS232Control();
+        currentHDG = 0.0;
+    }
+    
+    /**
+     * Tries to read a line from the serial port
+     *
+     * @return The line read from the serial port
+     */
+    protected String read() {
+        byte [] readArray = controller.testRead();
+        String readString = new String(readArray);
+        return readString;
+    }
 }
