53af96d1b927bf951cb6a0bbc679ba374f6598cb
ian5142 <ian@birdisland.net>
2022-02-18 15:16:13 -0400

    Added SerialPortReader class

--- src/javapilot/NMEA_Data_IO.java
+++ src/javapilot/NMEA_Data_IO.java
@@ -27,4 +27,50 @@ public class NMEA_Data_IO {
         reader = new SerialPortReader();
         readLine = "";
     }
+    
+    /**
+     * In this class must implement the method serialEvent, through it we learn
+     * about events that happened to our port. But we will not report on all
+     * events but only those that we put in the mask. In this case the arrival
+     * of the data and change the status lines CTS and DSR
+     */
+    private class SerialPortReader implements SerialPortEventListener {
+
+        /**
+         * Reads the data bit by bit from the serial port Can throw a
+         * SerialPortException error
+         *
+         * @param event
+         */
+        @Override
+        public void serialEvent(SerialPortEvent event) {
+            if (event.isRXCHAR() && event.getEventValue() == 10) {
+                try {
+                    String line = serialPort.readString(event.getEventValue());
+//                    acknowledgeStr + acknowledgeStr + 
+                    System.out.println("serialEvent: " + line);
+                    if (line.contains((char) 0x6 + "")) {
+                        System.out.println("Acknowledged");
+                        
+                    }
+                    else {
+                        
+                    }
+//                    System.out.println("Received response: " + readLine);
+               
+                } catch (SerialPortException ex) {
+                    System.out.println("Error in receiving string from COM-port: " + ex);
+                }
+            }
+        }
+    }
+
+    /**
+     * Prints out the message read from the serial port
+     *
+     * @param message
+     */
+    protected void processMessage(String message) {
+//        System.out.println(message);
+    }
 }
