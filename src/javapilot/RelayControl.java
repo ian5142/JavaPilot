fe4bcad1edba343f7d6e6f43c9cc7e32b7301fce
ian5142 <ian@birdisland.net>
2022-03-25 10:38:24 -0300

    Changes in RelayControl, switched to Diozero library. Fully tested code. Works fine.

--- src/javapilot/RelayControl.java
+++ src/javapilot/RelayControl.java
@@ -17,30 +17,24 @@
  */
 package javapilot;
 
-import com.pi4j.Pi4J;
-import com.pi4j.context.Context;
-import com.pi4j.io.gpio.digital.DigitalOutput;
-import com.pi4j.io.gpio.digital.DigitalState;
-import com.pi4j.util.Console;
-import java.util.concurrent.TimeUnit;
+import com.diozero.api.DigitalOutputDevice;
 
 /**
  * Contains methods to control the relays via GPIO on the Raspberry PI 4.
  * @author Ian
  */
 public class RelayControl {
-    public static final int DIGITAL_OUTPUT_PIN = 4;
-    public static final int DIGITAL_OUTPUT_PIN2 = 14;
-    Context pi4j = Pi4J.newAutoContext();
-    DigitalOutput leftOutput;
-    DigitalOutput rightOutput;
-    Console console;
+    public static final int DIGITAL_OUTPUT_PIN = 2; //Left Relay control pin
+    public static final int DIGITAL_OUTPUT_PIN2 = 3; //Right Relay control pin
+    DigitalOutputDevice leftOutput; //DigitalOutputDevice object used to control the left output pin
+    DigitalOutputDevice rightOutput; //DigitalOutputDevice object used to control the right output pin
     
     /**
      * The Relay Control Constructor
      */
     public RelayControl () {
-        startPI4J ();
+        leftOutput = new DigitalOutputDevice(DIGITAL_OUTPUT_PIN, false, true);
+        rightOutput = new DigitalOutputDevice(DIGITAL_OUTPUT_PIN2, false, true);
     }
     
     /**
@@ -48,71 +42,44 @@ public class RelayControl {
      * @param turnDirection The turn direction, 0 for left, 1 for right. Default is 0.
      * @return 
      */
-    protected int RelayON (int turnDirection) {
-        int error = 0;
+    protected boolean RelayON (int turnDirection) {
+        boolean relayON = false;
         if (turnDirection != 0 && turnDirection != 1) { // to handle if turn direction is something other than 0 or 1
             turnDirection = 0; // Turn on Left Relay
         }
         
         if (turnDirection == 0) { // Turn on Left Relay
-            leftOutput.low();
+            leftOutput.on();
+            relayON = leftOutput.isOn();
         }
         else if (turnDirection == 1) { // Turn on Right Relay
-            rightOutput.low();
+            rightOutput.on();
+            relayON = rightOutput.isOn();
         }
-        
-        System.out.print("CURRENT DIGITAL OUTPUT [" + leftOutput + "] STATE IS [");
-        System.out.println(leftOutput.state() + "]");
-        
-        // pulse to HIGH state for 3 seconds
-        System.out.println("PULSING OUTPUT STATE TO HIGH FOR 3 SECONDS");
-        leftOutput.pulse(3, TimeUnit.SECONDS, DigitalState.HIGH);
-        System.out.println("PULSING OUTPUT STATE COMPLETE");
 
-        return error;
+        return relayON;
     }
     
-    protected int RelayOFF (int turnDirection) {
-        int error = 0;
+    /**
+     * Turns the relay off.
+     * @param turnDirection The turn direction, 0 for left, 1 for right. Default is 0.
+     * @return True if relay is still ON, false if relay is successfully turned off.
+     */
+    protected boolean RelayOFF (int turnDirection) {
+        boolean relayON = true;
         if (turnDirection != 0 && turnDirection != 1) { // to handle if turn direction is something other than 0 or 1
             turnDirection = 0;
         }
         
         if (turnDirection == 0) { // Turn on Left Relay
-            leftOutput.high();
+            leftOutput.off();
+            relayON = leftOutput.isOn();
         }
         else if (turnDirection == 1) { // Turn on Right Relay
-            rightOutput.high();
+            rightOutput.off();
+            relayON = rightOutput.isOn();
         }
-        return error;
-    }
-    
-    protected void startPI4J () {
-        console = new Console();
-        // create Pi4J console wrapper/helper
-        // (This is a utility class to abstract some of the boilerplate stdin/stdout code)
-        
-        // print program title/header
-        console.title("<-- The Pi4J Project -->", "Basic Digital Output Example");
-
-        // allow for user to exit program using CTRL-C
-        console.promptForExit();
-        
-        // setup a digital output listener to listen for any state changes on the digital output
-        leftOutput.addListener(System.out::println);
-        rightOutput.addListener(System.out::println);
-        
-        // create a digital output instance using the default digital output provider
-        leftOutput = pi4j.dout().create(DIGITAL_OUTPUT_PIN);
-        leftOutput.config().shutdownState(DigitalState.HIGH);
-        rightOutput = pi4j.dout().create(DIGITAL_OUTPUT_PIN2);
-        rightOutput.config().shutdownState(DigitalState.HIGH);
-    }
-    
-    protected void shutdownPI4J () {
-        // shutdown Pi4J
-        console.println("ATTEMPTING TO SHUTDOWN/TERMINATE THIS PROGRAM");
-        pi4j.shutdown();
+        return relayON;
     }
 }
 
